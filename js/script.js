1a45dea708b146d334e18957478256eb09954c89
Eрматова Нургул <87805458+nurgulermatova@users.noreply.github.com>
2021-09-14 23:23:46 +0300

    add file

---
+++ js/script.js
@@ -0,0 +1,6 @@
+alert("Я начинаю изучать JS");
+ console.log("Nurgul"+"Ermatova");
+ console.log(100+100);
+ console.log(500*5);
+ document.write("<h1>Привет!</h1><p>Это мой первый скрипт!</p>");
+ document.write("100 + 100 = " + (100 + 100));
